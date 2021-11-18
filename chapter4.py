9beb80bd614cf73a9ba5f8b6b0c81ea376d2c4b9
EVAHI <wjli116@126.com>
2021-11-18 16:10:31 +0800

    chapter

--- chapter4.py
+++ chapter4.py
@@ -4,6 +4,7 @@
 #-*- encoding：utf-8 -*-
 
 import numpy as np
+from pandas.core.frame import DataFrame
 
 #使用numpy创建一个同时包含字符串、浮点数和整数的数组，用于存储股票行情数据时，需要按以下方式自定义dtype来实现
 deftype = ([('date',  np.str_, 10), ('close', np.float32), ('vol', np.uint32)])
@@ -78,17 +79,91 @@ df_list_list = pd.DataFrame([[1., 2., 3., 5], [1., 2., 3., 4.]],
                             columns = ['Close', 'Open', "low", 'High'])
 print(df_list_list)
 
+#以二维ndarray形式创建DataFrame
 ndarray_data = np.zeros((2), dtype = [('Close', 'i4'), ('Open', 'f4'), ('Low', 'a10')])
+print(np.zeros(2))
+print(np.zeros(shape=(2, 4)))
 print(ndarray_data)
 
 ndarray_data[:] = [(1, 2., '11.2'),  (2, 3., "12.3")]
 df_ndarray = pd.DataFrame(data = ndarray_data, index = ['2019-01-11', '2019-01-12'])
 print(df_ndarray)
 
+#以Series组成的字典形式创建DataFrame
 series_data = {'Close' : pd.Series([1., 2., 3.], index = ['2019-01-11', '2019-01-12', '2019-01-13']),
                'Open' : pd.Series([1., 2., 3., 4.], index = ['2019-01-11', '2019-01-12', '2019-01-13', '2019-01-14'])}
 df_series = pd.DataFrame(series_data)
 print(df_series)
 
+#以字典的列表形式创建DataFrame
 df_dict_list = pd.DataFrame([{'Close':1, 'Open':2}, {'Close':5, 'Open':10, 'High':20}], index = ['2019-01-11', '2019-01-12'])
 print(df_dict_list)
+
+#4.3.2 DataFrame的索引访问
+
+#以Series组成的字典形式创建DataFrame
+series_data = {'Close' : pd.Series([10.51, 10.52, 10.53, 10.54], index=['2019-01-11', '2019-01-12', '2019-01-13', '2019-01-14']),
+                'Open' : pd.Series([12.31, 12.32, 12.33, 12.34], index=['2019-01-11', '2019-01-12', '2019-01-13', '2019-01-14'])}
+df_access = pd.DataFrame(series_data)
+print(df_access)
+
+#访问DataFrame全部的行索引
+print(df_access.index)
+
+#访问DataFrame全部的列索引
+print(df_access.columns)
+
+#访问DataFrame全部的行和列索引
+print(df_access.axes)
+
+#访问DataFrame全部元素值
+print(df_access.values)
+
+#访问DataFrame某列的内容
+print(df_access['Open'])
+print(df_access.Open)
+
+#查看列类型
+print(type(df_access.Open))
+
+#访问某一行内容
+print(df_access[0:1])
+print(df_access[0:2])
+print(df_access[1:2])
+
+#选取了’2019-01-11‘行对应的’Close‘，’Open‘这两列的元素内容
+print(df_access.loc[['2019-01-11',], ['Close', 'Open']])
+
+#选取了所有行以及列索引为’Close‘，’Open‘的元素内容
+print(df_access.loc[:,['Close', 'Open']])
+
+#访问到'2019-01-11'这行的元素
+print(df_access.loc['2019-01-11'])
+
+#选取了前两行，第一列的元素
+print(df_access.iloc[0:2, 0:1])
+
+#选取了前两行，所有列的元素
+print(df_access.iloc[0:2])
+
+#访问第1行和第3行，第1列和第2列的元素
+print(df_access.iloc[[0,2],[0,1]])
+
+#采用混合标签和位置的方式访问元素 从’Open‘列索引中获取第0个和第2个元素
+print(df_access.ix[[0, 2], ['Open']])
+
+#
+print(df_access.index[[0, 2]])
+print(df_access.loc[df_access.index[[0, 2]], ['Open']])
+
+print(df_access.columns.get_indexer(['Open']))
+print(df_access.columns.get_loc('Open'))
+print(df_access.iloc[[0, 2], df_access.columns.get_indexer(['Open'])])
+print(df_access.index.get_loc('2019-01-12'))
+
+#4.3.5 用条件表达式访问元素
+
+#选取条件是‘Open'列中大于该列平均值，表达式运算后会产生一个True/False值的布尔型Series对象
+print(df_access.Open > df_access.Open.mean())
+
+print(df_access[df_access.Open > df_access.Open.mean()])
